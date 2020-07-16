441b1603659f035743df2c4dee2b5faf03eebd4b
xzy <xzy__1997@163.com>
2020-07-16 16:20:08 +0800

    二分图dfs

---
+++ DFS/785. 判断二分图.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int vNum = graph.size();
+        vector<int> visited(vNum,0);
+        for(int i=0;i<vNum;i++){
+            if(visited[i] == 0){
+                int r = dfs(graph,visited,i,1);
+                if(r == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+    int dfs(vector<vector<int>>& graph, vector<int>& visited, int vertex, int whichpart){
+        if(visited[vertex]){
+            return visited[vertex] == whichpart?1:0;
+        }
+        visited[vertex] =  whichpart;
+        for(int c:graph[vertex]){
+            int r = dfs(graph, visited, c, (whichpart % 2) + 1);
+            if(r == 0)
+                return 0;
+        }
+        return 1;
+    }
+};
